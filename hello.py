280f505269441c48f042aa45d71a398109b6d8ff
darthkrallt <darthkrallt@gmail.com>
2015-03-27 22:17:12 -0700

    Added a test user retrieval and generation endpoints. Still just test stuff, no real DB interaction.

--- hello.py
+++ hello.py
@@ -1,8 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, abort
+from userlib import generator
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/todo/api/v1.0/user_exists/<string:user_id>', methods=['GET'])
+def user_exists(user_id):
+    gen = generator.Generator()
+    user = gen.user_exists(user_id)
+    if not user:
+        abort(404)
+    return jsonify({'user_id': user})
+
+@app.route('/todo/api/v1.0/user', methods=['GET'])
+def gen_user():
+    gen = generator.Generator()
+    user_id = gen.make_user()
+    return jsonify({'user_id': user_id})
+
---
+++ userlib/generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import uuid
+
+"""
+   Userlib.generator
+        Generate new user that does not already exist in the DB.
+"""
+class Generator(object):
+    def __init__(self):
+        # connect to DB
+        pass
+        # testing junk
+        self.users = ['testuser', 'alice', 'bob']
+
+    def make_user(self):
+        # TODO: check this against DB
+        return uuid.uuid4()
+
+    def user_exists(self, user_id):
+        # testing junk
+        if user_id in self.users:
+            return user_id
